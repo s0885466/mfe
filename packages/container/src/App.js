8232848312a854afe7e6d620b5c5f8f0117103b7
Алиновский Александр Игоревич <Alinovskiy.A.I@sberbank.ru>
2021-01-05 13:04:25 +0300

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+  return <>
+      <h1>Hi this is a container</h1>
+      <hr/>
+      <MarketingApp/>
+    </>
+};
